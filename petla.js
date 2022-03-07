125a8db914c5c0e731ccd28d7234a8a2e199a80f
Pawel Drabato <pawel.drab@gmail.com>
2022-03-07 22:02:45 +0100

    something new

---
+++ petla.js
@@ -0,0 +1,43 @@
+//przykład
+// for (let i = 0; i < 5; i++){
+//     console.log(i);
+// }
+
+// zadanie 1 artykuł Petle for
+//kolejność wprowadzania danych do pętli to a) for(inicjalizator ; warunek; itertor)
+// zadanie 2
+// for (let i = 0; i <= 15; i++){
+//     console.log(i)
+// }
+// zadanie 3
+// const array = [0, 1, 2, 3, 4, 5]
+// for (let i = 5; i >=0; i--){
+//     console.log(i)
+// }
+// zadanie 4
+// let number = 5
+// let sum = 0
+// for (let i = sum; i <= number; i ++){
+//     sum +=i
+// }
+// console.log(sum)
+
+// // zadanie 5
+
+// const array = [0, 0, 0, 0, 0,]
+// for (i = 0; i < array.length; i++){
+//     console.log(i+array[i])
+// }
+
+// const array2 = [5, 4, 3, 2, 1,]
+// for (i = 0; i < array2.length; i++){
+//     console.log(i+array2[i])
+// }
+
+// zadanie 6
+const array3 = [true, false, false, true, false, true, false, false,]
+for (i = 0; i < array3.length; i++){
+    if(array3[i] === true){console.log(1)} else {console.log(0)}
+}
+
+const array4 = ["Styczeń 22", "Luty 15", "Luty 12", "Marzec 4", "Marzec 8", "Marzec 9"]
